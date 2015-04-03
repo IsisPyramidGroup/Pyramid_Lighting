32ad3d662291c2211842479810f00188d1199171
Paul Williamson <paul@mustbeart.com>
2015-04-03 16:34:45 -0700

    Added time reset and comment functions to the LP compiler. Added convenient aliases for entity bitmap addresses to the LP compiler. Created ROVERV (vertical roving white pixel) test LP.

--- lighting programs/lpcompile.py
+++ lighting programs/lpcompile.py
@@ -16,12 +16,29 @@
 import sys
 
 ALL = 0xFFFF        # all-call address for either slaves or entities
+DIAGS = 0x00FF      # address for all the diagonals
+SIDES = 0x0F00      # address for all the bottom edges
+
+# Entity names
+E_DIAG_0 = 0x0001
+E_DIAG_1 = 0x0002
+E_DIAG_2 = 0x0004
+E_DIAG_3 = 0x0008
+E_DIAG_4 = 0x0010
+E_DIAG_5 = 0x0020
+E_DIAG_6 = 0x0040
+E_DIAG_7 = 0x0080
+E_LEFT   = 0x0100
+E_BACK   = 0x0200
+E_RIGHT  = 0x0400
+E_FRONT  = 0x0800
 
 # Slave packet command codes
 CMD_S_RESET_CLOCK = 0x00
 CMD_S_DYN_BLINK   = 0x01
 CMD_S_DYN_THROB   = 0x02
 CMD_S_DYN_SPARKLE = 0x03
+CMD_S_COMMENT	  = 0x04
 
 # Entity packet command codes
 CMD_E_FILL_RGB      = 0xC0
@@ -36,7 +53,8 @@ CMD_E_RAINBOW       = 0xC7
 META = 0xFF         # special command code that is interpreted locally and not sent
 META_CONSOLE = 0x00     # subcommands of the META command code
 META_WAIT    = 0x01
-META_ENDSAT  = 0x02
+META_ENDS    = 0x02
+META_RESET_TIME = 0x03
 
 # special codes for byte stuffing, per KISS or SLIP protocol standards.
 FEND = 0xC0
@@ -71,7 +89,13 @@ def ends_at_tick(tick):
     """Insert a meta packet instructing the master that the current program, if run to
     completion, will end when the tick number reaches a certain value.
     """
-    write_packet([META, META_WAIT, LO(tick), HI(tick)])
+    write_packet([META, META_ENDS, LO(tick), HI(tick)])
+
+def reset_master_clock():
+	"""Insert a meta packet instructing the master to reset its own version of time
+	at this point in the program.
+	"""
+	write_packet([META, META_RESET_TIME])
     
 def write_packet(bytes):
     """Write a list of bytes into the output file, with SLIP framing.
@@ -112,6 +136,14 @@ def cmd_s_dyn_sparkle(bitmap, probability):
     """Insert a DYN_SPARKLE packet addressed to the slaves shown in bitmap.
     """
     write_packet([CMD_S_DYN_SPARKLE, LO(bitmap), HI(bitmap), probability])
+
+def comment(string):
+	"""Insert a comment packet, nominally addressed to all slaves. This does nothing
+	at the slave, but inserts some readable text into the bitstream for debug.
+	"""
+	packet = [CMD_S_COMMENT, 255, 255]
+	packet += string[:11]
+	write_packet(packet)
 
 def cmd_e_fill_rgb(bitmap, repeat_count, start_tick, repeat_interval, red, green, blue):
     """Insert a CMD_E_FILL_RGB packet with the specified contents.
---
+++ lighting programs/roverv.py
@@ -0,0 +1,13 @@
+# Test of shifting up and down on entities!
+# Also uses LOADONE and FILL_RGB
+# Needs whole pyramid to validate visually
+filename("ROVERV.PKT")
+comment("Rover V")
+cmd_s_reset_clock(ALL)
+reset_master_clock()
+step = 10		# 10 ticks = 100ms
+cmd_e_fill_rgb(ALL, 1, 0, 0, 0, 0, 0)		# fill everything with black
+cmd_e_loadone(DIAGS, 1, 0, 0, 67, 255,255,255, 0)	# start at the top
+cmd_e_shift_down(DIAGS, 67, step, step, 1, 0,0,0, 0)	# run them down to the bottom
+cmd_e_shift_up(DIAGS, 66, 68*step, step, 1, 0,0,0, 0)	# and then back up
+ends_at_tick(134*step)
